55ce16addf2999b75d71dbccfc206e1cfb1b4099
integrationninjas <integrationninjas@gmail.com>
2024-01-22 23:20:17 +0530

    test1

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
   const [formData, setFormData] = useState({ name: "", email: "" });
   const [recordData, setRecordData] = useState([]);
 
+  console.log("process.env:", process.env);
   console.log("process.env.REACT_APP_NODE_ENV:", process.env.REACT_APP_NODE_ENV);
   console.log("process.env.REACT_APP_SERVER_BASE_URL:", process.env.REACT_APP_SERVER_BASE_URL);
   const base_url = process.env.REACT_APP_NODE_ENV === 'development' ? process.env.REACT_APP_LOCAL_BASE_URL : process.env.REACT_APP_SERVER_BASE_URL;
